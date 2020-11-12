0958c43d26e1bd3465a0981ad8c2ed9184043fae
pongdanny <pongdaniel17@gmail.com>
2020-11-12 15:49:48 -0500

    setup

---
+++ src/components/Rainbow.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route, Link, NavLink } from "react-route-dom";
+
+const Rainbow = () => (
+  <div>
+    <h1>Rainbow Router</h1>
+    <div id="rainbow"></div>
+  </div>
+);
+
+export default Rainbow;
